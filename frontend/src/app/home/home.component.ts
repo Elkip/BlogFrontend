af3fc177a48e76eb43500e224463c3f65e187e22
elkip <mitchellhenschel@gmail.com>
2023-01-17 13:52:50 -0500

    Front page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -13,15 +13,14 @@ export class HomeComponent implements OnInit {
     const body = document.body;
     const box = document.getElementById('box');
     const mouseHandler = e => {
-      const docW = body.clientWidth / 2;
+      const docW = body.clientWidth / 1.1;
       const docH = body.clientHeight / 2;
-      const moveX = (e.clientX - docW) / 10;
-      const moveY = (e.clientY - docH) / 10;
-      box.style.transform = 'translate(' + moveX / 20 + 'px, ' + moveY / 20 + 'px)';
-      box.style.transform += 'rotateX(' + -moveY / 2 + 'deg)';
-      box.style.transform += 'rotateY(' + moveX / 2 + 'deg)';
+      const moveX = (e.clientX - docW) / 6;
+      const moveY = (e.clientY - docH) / 6;
+      box.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)';
+      // box.style.transform += 'rotateX(' + -moveY / 2 + 'deg)';
+      // box.style.transform += 'rotateY(' + moveX / 2 + 'deg)';
     };
     body.addEventListener('mousemove', mouseHandler, false);
   }
-
 }
